b68511e418ae4dadb2eb77556c8501726af3ed03
leena <leena@cybertekschool.com>
2023-06-01 20:45:58 +0300

    Solutions are added

---
+++ src/week_1/question_1/Leena.java
@@ -0,0 +1,9 @@
+package week_1.question_1;
+
+public class Leena {
+    public static String identify(int num){
+        if (num%2==0)
+            return "Even";
+        return "Odd";
+    }
+}
---
+++ src/week_1/question_3/Leena.java
@@ -0,0 +1,17 @@
+package week_1.question_3;
+
+public class Leena {
+
+    public static void finraNumbers() {
+        for (int i = 1; i <= 30; i++) {
+            if (i % 15 == 0)
+                System.out.println("FINRA");
+            else if (i % 5 == 0)
+                System.out.println("RA");
+            else if (i % 3 == 0)
+                System.out.println("FIN");
+            else
+                System.out.println(i);
+        }
+    }
+}
