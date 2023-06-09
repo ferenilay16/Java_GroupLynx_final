620529fc9e3929ecefba134538472559c36f6f2a
leena <leena@cybertekschool.com>
2023-06-09 22:30:57 +0300

    week4 questions are added

--- src/week_3/question1/Leena.java
+++ src/week_3/question1/Leena.java
@@ -7,4 +7,41 @@ public class Leena {
     Write a method that can check if a number is prime or not
 
      */
+    public static void main(String[] args) {
+        checkNumber(4);
+
+    }
+    public static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public static void checkNumber(int n){
+
+        if(n>=2){
+            String result= n+" is a prime number";
+            for (int i=2;  i<n ; i++) {
+                if(n%i==0){
+
+                    result=n+" is Not a prime number";
+                }
+            }
+            System.out.println(result);
+
+        }
+
+
+
+    }
+
 }
