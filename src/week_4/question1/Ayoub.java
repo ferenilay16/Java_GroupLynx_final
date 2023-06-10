13651a6dfee47753174c1abe943a42d6b0240c04
ayoub <Ayoub.hanssali95@gmail.com>
2023-06-09 21:54:37 -0500

    update

---
+++ src/week_4/question1/Ayoub.java
@@ -0,0 +1,13 @@
+package week_4.question1;
+
+public class Ayoub {
+
+
+
+}
+/*
+String -- Frequency of Characters
+Write a return method that can find the frequency of
+characters
+Ex: FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+ */
