ca65df1acfbc42ff7955c962571d981cda8c6526
medetnussipbek <m.nussipbek@gmail.com>
2023-06-15 10:20:15 -0500

    Question 3 Remove Duplicates

---
+++ src/week_4/Question3/Medet.java
@@ -0,0 +1,44 @@
+package week_4.Question3;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Medet {
+
+    public static String removeDuplicates(String str) {
+        // Create a Set to store unique characters in the order of their appearance
+        Set<Character> uniqueChars = new LinkedHashSet<>();
+
+        // Iterate over each character in the string
+        for (char c : str.toCharArray()) {
+            // Add the character to the Set
+            uniqueChars.add(c);
+        }
+
+        // Construct the resulting string from the unique characters in the Set
+        StringBuilder result = new StringBuilder();
+        for (char c : uniqueChars) {
+            result.append(c);
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        scanner.close();
+
+        String result = removeDuplicates(input);
+        System.out.println("Result: " + result);
+    }
+}
+
+/*String -- Remove Duplicates
+Write a return method that can remove the duplicated values from String
+Ex: removeDup("AAABBBCCC") ==> ABC
+*/
