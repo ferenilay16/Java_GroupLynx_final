dd60d55f5cb3b6e6d2136870bf5a8f8e78939dd0
ayoub <Ayoub.hanssali95@gmail.com>
2023-06-22 09:50:43 -0500

    update

--- src/week_5/Question2_ReverseString/Ayoub.java
+++ src/week_5/Question2_ReverseString/Ayoub.java
@@ -1,4 +1,19 @@
 package week_5.Question2_ReverseString;
 
 public class Ayoub {
+    public static void main(String[] args) {
+        System.out.println("reverse(\"ABCD\") = " + reverse("ABCD"));
+    }
+    public static String reverse(String str){
+        String reverseWord="";
+        for (int i =str.length()-1; i>=0 ; i--) {
+            reverseWord+=str.charAt(i);
+        }
+        return reverseWord;
+    }
+
 }
+ /*
+    Write a return method that can reverse  String
+Ex: Reverse("ABCD"); ==> DCBA
+     */
