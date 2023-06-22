5cb5cfa0cfea7fc200df664c7433b5592b09cc9f
zeynepkortak <123099181+zeynepkortak@users.noreply.github.com>
2023-06-21 21:43:36 -0500

    week 5 question 2 zeynep

--- src/week_5/Question1_UniqueChars/zeynep.java
+++ src/week_5/Question1_UniqueChars/zeynep.java
@@ -3,7 +3,7 @@ package week_5.Question1_UniqueChars;
 public class zeynep {
     /*
     Write a return  method that can find the unique characters from the String
-    Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";
+    Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";;
      */
 
     public static String findUniqueCharacters(String str) {
