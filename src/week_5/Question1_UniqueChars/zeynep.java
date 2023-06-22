3c6998147d0a7164906dd7d40701655848ab817c
zeynepkortak <123099181+zeynepkortak@users.noreply.github.com>
2023-06-21 21:34:16 -0500

    week 5 question 1 zeynep

---
+++ src/week_5/Question1_UniqueChars/zeynep.java
@@ -0,0 +1,36 @@
+package week_5.Question1_UniqueChars;
+
+public class zeynep {
+    /*
+    Write a return  method that can find the unique characters from the String
+    Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";
+     */
+
+    public static String findUniqueCharacters(String str) {
+        String unique = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char each = str.charAt(i);
+
+            if (str.indexOf(each) == str.lastIndexOf(each)) {
+                unique += each;
+            }
+        }
+
+        return unique;
+    }
+
+    public static void main(String[] args) {
+
+        String str = "AAABBBCCCDEF";
+        String unique = findUniqueCharacters(str);
+        System.out.println(unique);
+
+    }
+
+
+
+
+
+
+}
