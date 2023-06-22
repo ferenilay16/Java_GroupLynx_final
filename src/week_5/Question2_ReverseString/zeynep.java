b97f1acd72bf5196077419c603ff3982c7ed4be1
zeynepkortak <123099181+zeynepkortak@users.noreply.github.com>
2023-06-21 21:40:03 -0500

    week 5 question 2 zeynep

--- src/week_5/Question2_ReverseString/zeynep.java
+++ src/week_5/Question2_ReverseString/zeynep.java
@@ -11,7 +11,7 @@ Ex: Reverse("ABCD"); ==> DCBA
         String reverse = ""; // to contain all the characters of the given string in reversed order
 
         for (int i = str.length()-1 ; i >= 0; i--) { // i: index number of the given string starting from last index to index 0
-            reverse += str.charAt(i); // to get each characters of the string starting from last index to index zero
+            reverse += str.charAt(i); // to get each character of the string starting from last index to index zero
 
         }
 
