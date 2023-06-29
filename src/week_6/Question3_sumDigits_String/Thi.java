9a30f0139cfe8a2efeae1a618ce002f71be77558
tleafconsultingco <tleaf.consultingco@gmail.com>
2023-06-29 12:05:23 -0500

    solutions week 6

--- src/week_6/Question3_sumDigits_String/Thi.java
+++ src/week_6/Question3_sumDigits_String/Thi.java
@@ -1,6 +1,29 @@
 package week_6.Question3_sumDigits_String;
 
 public class Thi {
+
+    public static void sumDigits(String str){
+        // int variable sum to hold the sum
+        int sum = 0;
+
+        // iterate through string
+        for (int i = 0; i < str.length(); i++) {
+            // char variable ch for each character at index i
+            char ch = str.charAt(i);
+            // use wrapper class to access isDigit() method and pass ch
+            if (Character.isDigit(ch)){
+                // store numerical value of ch in int number variable
+                int number = Character.getNumericValue(ch);
+                // update sum variable by adding number value
+                sum += number;
+            }
+        }
+        System.out.println(sum);
+        }
+
+    public static void main(String[] args) {
+        sumDigits("a1b20c35");
+    }
 }
 /*
     Sum of digits in a string:
