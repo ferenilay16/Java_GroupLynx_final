35508e78919149f495831c4a2569ba1288ab4eda
ayoub <Ayoub.hanssali95@gmail.com>
2023-07-06 12:23:39 -0500

    update

--- src/week_7/Question2/Ayoub.java
+++ src/week_7/Question2/Ayoub.java
@@ -1,35 +1,32 @@
 package week_7.Question2;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
-
 public class Ayoub {
-    public static void main(String[] args) {
 
-        int[] arr = {10,7,5, 8, 7};
-        int [] newArray =new int [3];
-        int minNumber =2147483647;
-        int maxNumber =-2147483647;
+    public static void newSortMethod(int[] arr){
+        for (int j = 0; j <arr.length-1 ; j++) {
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
 
-        for (int i = 0; i <arr.length; i++) {
+                    int n = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = n;
 
-            if(arr[i] >maxNumber){
-                maxNumber=arr[i];
+                }
+
             }
-            if(arr[i] <minNumber){
-                minNumber=arr[i];
-            }
-
         }
-        System.out.println("maxNumber = " + maxNumber);
-        System.out.println("minNumber = " + minNumber);
+
+        System.out.println(Arrays.toString(arr));
+    }
 
 
-
+    public static void main(String[] args) {
+        int[] list = {10, 91,0,2, 8, 7};
+        newSortMethod(list);
 
 
     }
-
 }
  /*
     Write a return method that can sort an int array in Ascending order without using the sort method of the Arrays class
