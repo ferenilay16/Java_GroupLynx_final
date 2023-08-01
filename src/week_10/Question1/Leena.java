a61e105f09c2ce7691aed71f1158fa6967d683ae
leena <leena@cybertekschool.com>
2023-08-01 19:46:11 +0300

    week10 task added

---
+++ src/week_10/Question1/Leena.java
@@ -0,0 +1,9 @@
+package week_10.Question1;
+
+public class Leena {
+    /*
+    ArrayList - sorting in ascending
+    Write a method that can sort the ArrayList in Ascending order without using
+    the sort method.
+     */
+}
---
+++ src/week_10/Question3/Leena.java
@@ -0,0 +1,9 @@
+package week_10.Question3;
+
+public class Leena {
+
+    /*
+    Map - Sort the map by values
+    Write a method that can sort the Map by values.
+     */
+}
